4b354e340224e92956c136b6aae966efc9b78eab
1101193 <a1101193@11ST1101193MN001.local>
2019-06-27 16:31:07 +0900

    modified object delete

--- springboot-gradle-aws/src/main/java/springbootgradleaws/springbootgradleaws/TestController.java
+++ springboot-gradle-aws/src/main/java/springbootgradleaws/springbootgradleaws/TestController.java
@@ -2,6 +2,7 @@ package springbootgradleaws.springbootgradleaws;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.regions.Region;
@@ -16,11 +17,15 @@ import java.util.Random;
 @RestController
 public class TestController {
 
-
     Region region = Region.AP_NORTHEAST_2;
-    S3Client s3Client = S3Client.builder().region(region).build();
+//    S3Client s3Client = S3Client.builder().region(region).build();
+
+    S3Client s3Client = S3Client.builder()
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
+
     String bucket = "11st-bucket-test";
-    String key = "index.html";
+    String key = "img.jpg";
 
     @RequestMapping(value = "/hello")
     public String hello() {
@@ -67,28 +72,34 @@ public class TestController {
         return "Delete Bucket";
     }
 
-    private static ByteBuffer getRandomByteBuffer(int size){
+    private static ByteBuffer getRandomByteBuffer(int size) {
         byte[] b = new byte[size];
         new Random().nextBytes(b);
         return ByteBuffer.wrap(b);
     }
 
+//    file upload
     @RequestMapping("/upload")
     public String uploadToBucket() throws IOException {
-
-
         s3Client.putObject(PutObjectRequest.builder().bucket(bucket).key(key).build(),
                 RequestBody.fromByteBuffer(getRandomByteBuffer(10_000)));
-
         return "Upload to Bucket";
     }
 
-    @RequestMapping("download")
+//    file download
+    @RequestMapping("/download")
     public String downloadToBucket() {
 
         s3Client.getObject(GetObjectRequest.builder().bucket(bucket).key(key).build(),
-                        ResponseTransformer.toFile(Paths.get("index.html")));
+                ResponseTransformer.toFile(Paths.get("img.jpg")));
         return "Download to Bucket";
     }
 
+//    file delete
+    @RequestMapping("/delfile")
+    public String deletefile(){
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(bucket).key(key).build();
+        s3Client.deleteObject(deleteObjectRequest);
+        return "Delete File";
+    }
 }
